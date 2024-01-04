623767e533b42af5adccabf96a19de770e5f4ff9
saranyadurga <saranyadurga@gmail.com>
2024-01-04 18:36:09 -0500

    kkk

---
+++ day_1/usecases/ex1.py
@@ -0,0 +1,15 @@
+#Method1(Using directly to list the elements)
+listVal = [11,12,15]
+len1=len(listVal)
+if len1 != 0:
+    for x in listVal:
+        print("Values in the list",x)
+        
+#Method2(for loop using range)
+listVal = [11,12,15]
+len1=len(listVal)
+if len1 != 0:
+    for x in range(len1):
+        print("Values in the list",listVal[x])       
+
+    
