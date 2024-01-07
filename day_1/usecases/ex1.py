67aac8b385771f3e84836adc712af20b90014109
saranyadurga <saranyadurga@gmail.com>
2024-01-06 23:59:27 -0500

    hhh

--- day_1/usecases/ex1.py
+++ day_1/usecases/ex1.py
@@ -3,7 +3,7 @@ listVal = [11,12,15]
 len1=len(listVal)
 if len1 != 0:
     for x in listVal:
-        print("Values in the list method2",x)
+        print("Values in the list method1",x)
 print("\n=======")
         
 #Method2(for loop using range)
@@ -11,6 +11,6 @@ listVal = [11,12,15]
 len1=len(listVal)
 if len1 != 0:
     for x in range(len1):
-        print("Values in the list",listVal[x])       
+        print("Values in the list method2",listVal[x])       
 
     
